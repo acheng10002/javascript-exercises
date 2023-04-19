71c1520b9a010102ac3e3ff2010f5c952be62f06
Amy Cheng <109977623+acheng10002@users.noreply.github.com>
2023-04-19 12:44:01 -0400

    Update and complete 08-09 js files

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,55 @@
-const add = function() {
+const add = (addendA, addendB) => {
+  {
+    addendA, 
+    addendB
+  }
+  return addendA + addendB;
+};
+
+const subtract = (minuend, subtrahend) => {
+  {
+    minuend,
+    subtrahend
+  }
+  return minuend - subtrahend;
+};
+
+const sum = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+};
+
+const multiply = function(array) {
+  let product = 1;
+  for (let i = 0; i < array.length; i++) {
+    product *= array[i];
+  }
+  return product;
+};
+
+const power = (leftOperand, rightOperand) => {
+  {
+    leftOperand,
+    rightOperand
+  }
+  return leftOperand ** rightOperand;
+};
+
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
+};
 	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let newWord = word.replace(/[^a-zA-Z]/g, '').toUpperCase();
+        // replace() replaces any character that is not a letter with an empty string 
+        // the regex `/[^a-zA-Z]/g` matches any character that is not a letter and the `g` flag
+        //  makes the replacement global
+    let reversedNewWord = newWord.split('').reverse().join('');
+        // split('') splits the string into an array of individual characters
+        // ...empty string `''` argument specifies that each character in the string should be
+        // ...treated as a separate element in the array
+        // reverse() reverses the order of the elements in the array
+        // join('') joins the elements in the array back into a string
+        // ...empty string `''` argument specifies that there should be no separator between the
+        // ...characters in the resulting string
+    if (newWord === reversedNewWord) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
